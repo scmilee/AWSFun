0b2e1e74a4e55990c76168a7d300c76e5f816c44
scmilee <drudged@oregonstate.edu>
2019-02-04 15:56:07 -0800

    change s3 bucket name to prevent conflicts

--- spoticry/src/renderer/lib/Spotify.js
+++ spoticry/src/renderer/lib/Spotify.js
@@ -64,7 +64,7 @@ export default class Spotify {
         if (err) {
           console.log(err)
         }
-        alert('Successfully uploaded photo.');
+        alert('Successfully uploaded stuff.');
         return data
       });
     }) 
