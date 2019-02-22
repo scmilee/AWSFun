d13a569317721a2a08c3b7e877660f5e6cd79726
Dylan Drudge <drudged@oregonstate.edu>
2019-02-21 17:48:32 -0800

    extract resolver and schema from server.js

--- spotifyservice/src/server.js
+++ spotifyservice/src/server.js
@@ -1,62 +1,17 @@
 import 'babel-polyfill'
-import spotifyS3 from './lib/Spotify.js'
 const express = require('express');
 const cors = require('cors');
 const express_graphql = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-const spotty = new spotifyS3();
+const schema = require('./schema/schema');
+const rootResolver = require('./resolver/rootResolver')
 
-const schema = buildSchema(`
-    type Query {
-        songs(title: String): [Song]
-    }
-    type Song {
-        key: String,
-        name: String,
-        album: String,
-        artist: String,
-        url: String
-    }
-`);
-//for testing 
-// let songs = [
-//     {
-//         key: "lolkey",
-//         name: "lols",
-//         album: "lol the album",
-//         artist: "lol the singer",
-//         url: "lol"
-//     },
-//     {
-//         key: "lolkey",
-//         name: "lols2",
-//         album: "lol the album",
-//         artist: "lol the singer",
-//         url: "lol"
-//     },
-//     {
-//         key: "lolkey",
-//         name: "lols",
-//         album: "lmao the album",
-//         artist: "lmao the singer",
-//         url: "lol"
-//     }
-// ]
-
-const getSongs = async(args) => {
-    return await spotty.getSongs(args); 
-}
-
-const root = {
-    songs: getSongs
-};
 const app = express();
 app.use(cors())
 
 app.use('/graphql', express_graphql({
     schema: schema,
-    rootValue: root,
+    rootValue: rootResolver,
     graphiql: true
 }));
 
