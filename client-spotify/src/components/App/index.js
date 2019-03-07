0ef509737b128ffdc22026174371652a61a56802
Dylan Drudge <drudged@oregonstate.edu>
2019-03-06 16:18:14 -0800

    random update to route stuffs

--- client-spotify/src/components/App/index.js
+++ client-spotify/src/components/App/index.js
@@ -15,15 +15,15 @@ import * as ROUTES from '../../constants/routes';
 
 const App = () => (
   <Router>
-  <div>
-    <Navigation />
-    <Route exact path={ROUTES.LANDING} component={LandingPage} />
-    {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-    <Route path={ROUTES.SIGN_IN} component={SignInPage} /> */}
-    <Route path={ROUTES.ACCOUNT} component={Account} />
-    <Route path={ROUTES.HOME} component={HomePage} />
-  </div>
-</Router>
+    <div>
+      <Navigation />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} /> */}
+      <Route path={ROUTES.ACCOUNT} component={Account} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+    </div>
+  </Router>
 )
 
 
